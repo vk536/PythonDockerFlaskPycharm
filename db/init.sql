cd5f6d7ec0a5a2d53214d603e3beb375d4c104f4
vivek <vk536@njit.edu>
2020-11-24 15:51:37 -0500

    Feature: Adding view and modifying SQL

--- db/init.sql
+++ db/init.sql
@@ -1,25 +1,24 @@
-CREATE DATABASE airTravel;
+create database airTravel;
 use airTravel;
 
 CREATE TABLE IF NOT EXISTS airTravelImport (
-    `id` int AUTO_INCREMENT,
-    `Month` VARCHAR(21) CHARACTER SET utf8,
-    `1958` INT,
-    `1959` INT,
-    `1960` INT,
-    PRIMARY KEY (`id`)
+    'ID' int AUTO_INCREMENT,
+    `Month` VARCHAR(3) CHARACTER SET utf8,
+    `Column_1958` INT,
+    `Column_1959` INT,
+    `Column_1960` INT
 );
-INSERT INTO tblCitiesImport (Month,1958,1959,1960) VALUES
-    ('JAN',340,360,417),
-    ('FEB',318,342,391),
-    ('MAR',362,406,419),
-    ('APR',348,396,461),
-    ('MAY',363,420,472),
-    ('JUN',435,472,535),
-    ('JUL',491,548,622),
-    ('AUG',505,559,606),
-    ('SEP',404,463,508),
-    ('OCT',359,407,461),
-    ('NOV',310,362,390),
-    ('DEC',337,405,432);
 
+INSERT INTO airTravelImport (Month, Column_1958, Column_1959, Column_1960) VALUES
+    ('JAN',  340,  360,  417),
+    ('FEB',  318,  342,  391),
+    ('MAR',  362,  406,  419),
+    ('APR',  348,  396,  461),
+    ('MAY',  363,  420,  472),
+    ('JUN',  435,  472,  535),
+    ('JUL',  491,  548,  622),
+    ('AUG',  505,  559,  606),
+    ('SEP',  404,  463,  508),
+    ('OCT',  359,  407,  461),
+    ('NOV',  310,  362,  390),
+    ('DEC',  337,  405,  432);
